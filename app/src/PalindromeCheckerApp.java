df711b1b3000ad4ef112071658dfc28745df6f90
YourGitHubUsername <rushi.dev404@gmail.com>
2026-02-23 16:35:39 +0530

    UC3- reverse string

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,15 +1,23 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-    System.out.println("Welcome to the Palindrome Checker App!");
-    System.out.println("Version : 1.0");
-    System.out.println("System initialized successfully.");
+
     String s="madam";
-    for(int i = 0; i < s.length/2; i++) {
-        if(s.charAt(i) != s.charAt(s.length-1-i)) {
+    for(int i = 0; i < s.length()/2; i++) {
+        if(s.charAt(i) != s.charAt(s.length()-1-i)) {
             System.out.println("Is it a palindrome? : false");
             return;
         }
     System.out.println("Is it a palindrome? : true");
     }
 
+    //UC3
+    String reversed = "";
+    for (int  i=s.length()-1; i>=0; i--){
+        reversed = reversed + s.charAt(i);
+    }
+    if (s.equals(reversed)){
+        System.out.println("Is it a palindrome? : true");
+    } else {
+        System.out.println("Is it a palindrome? : false");
+    }
 }}
