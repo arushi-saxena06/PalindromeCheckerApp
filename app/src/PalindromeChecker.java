62c954aa54e4502fde604830207b24d4a3df11e3
YourGitHubUsername <rushi.dev404@gmail.com>
2026-03-10 13:27:26 +0530

    ADDED UC5

---
+++ app/src/PalindromeChecker.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
